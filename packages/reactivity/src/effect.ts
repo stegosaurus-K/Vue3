b4184af1f01e4aadfab2bdb0f7f10d57ea2968c9
范剑 <331861471@qq.com>
2025-12-22 23:49:01 +0800

    feat：分支切换和依赖清理

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,7 +1,18 @@
+import { endTrack, Link, startTrack } from './system'
+
 // 用来保存当前正在执行的effect
 export let activeSub
 
 class ReactiveEffect {
+  /**
+   * 依赖项链表的头节点
+   */
+  deps: Link | undefined
+  /**
+   * 依赖项链表的尾节点
+   */
+  depsTail: Link | undefined
+
   constructor(public fn) {}
 
   run() {
@@ -10,9 +21,12 @@ class ReactiveEffect {
 
     // 每次执行fn之前，把this放到activeSub上面
     activeSub = this
+    // depsTai标记为undefined， 表示被dep触发了重新执行，要尝试复用lin节点
+    startTrack(this)
     try {
       return this.fn()
     } finally {
+      endTrack(this)
       // 执行完成后，把activeSub恢复成之前的effect
       activeSub = prevSub
     }
@@ -46,4 +60,3 @@ export function effect(fn, option) {
   runner.effect = e
   return runner
 }
- 
--- packages/reactivity/src/system.ts
+++ packages/reactivity/src/system.ts
@@ -1,34 +1,85 @@
+/**
+ * 依赖项
+ */
+interface Dep {
+  //订阅者链表的头节点
+  subs: Link | undefined
+  // 订阅者链表的尾节点
+  subsTail: Link | undefined
+}
+interface Sub {
+  //订阅者链表的头节点
+  deps: Link | undefined
+  // 订阅者链表的尾节点
+  depsTail: Link | undefined
+}
+/**
+ * 链表节点
+ */
 export interface Link {
-  // 保存 effect
-  sub: Function
+  // 订阅者
+  sub: Sub
   nextSub: Link | undefined // 下一个节点
   prevSub: Link | undefined // 上一个节点
+
+  // 依赖项
+  dep: Dep
+
+  nextDep: Link | undefined
 }
 
 /**
  * 建立链表关系
- * @param dep 
- * @param sub 
+ * @param dep
+ * @param sub
  */
 export function link(dep, sub) {
-  const newSub = {
+  /**
+   * 尝试复用链表节点
+   * 1. 如果头节点有，尾节点没有，尝试复用头节点
+   * 2. 如果尾节点还有nextDep，尝试复用尾节点的nextDep
+   */
+  const currentDep = sub.depsTail
+  const nextDep = currentDep === undefined ? sub.deps : currentDep.nextDep
+  if (nextDep && nextDep.dep === dep) {
+    sub.depsTail = nextDep
+    return
+  }
+
+  const newLink = {
     sub,
+    dep,
+    nextDep,
     nextSub: undefined,
     prevSub: undefined,
   }
+  /**
+   * 将链表节点和dep建立关联关系
+   */
   if (dep.subsTail) {
-    dep.subsTail.nextSub = newSub
-    newSub.prevSub = dep.subsTail
-    dep.subsTail = newSub
+    dep.subsTail.nextSub = newLink
+    newLink.prevSub = dep.subsTail
+    dep.subsTail = newLink
   } else {
-    dep.subs = newSub
-    dep.subsTail = newSub
+    dep.subs = newLink
+    dep.subsTail = newLink
+  }
+
+  /**
+   *
+   */
+  if (sub.depsTail) {
+    sub.depsTail.nextDep = newLink
+    sub.depsTail = newLink
+  } else {
+    sub.deps = newLink
+    sub.depsTail = newLink
   }
 }
 
 /**
  * 传播更新的函数
- * @param subs 
+ * @param subs
  */
 export function propagate(subs) {
   let link = subs
@@ -38,4 +89,67 @@ export function propagate(subs) {
     link = link.nextSub
   }
   queueEffects.forEach(effect => effect.notify())
+}
+/**
+ * 开始追踪依赖，将depsTail尾节点设置成undefined
+ * @param sub
+ */
+export function startTrack(sub) {
+  sub.depsTail = undefined
+}
+/**
+ * 结束追踪，找到需要清理的依赖，断开关联关系
+ * @param sub
+ */
+export function endTrack(sub) {
+  const depsTail = sub.depsTail
+  /**
+   * depsTail 有，并且depsTail还有nextDep，应该把他们的依赖关系清理掉
+   * depsTail 没有，并且头节点有， 那就把所有的依赖关系都清理掉
+   *
+   */
+  if (depsTail) {
+    if (depsTail.nextDep) {
+      clearTracking(depsTail.nextDep)
+      depsTail.nextDep = undefined
+    }
+  } else if (sub.deps) {
+    clearTracking(sub.deps)
+    sub.deps = undefined
+  }
+}
+/**
+ * 清理依赖关系
+ * @param link
+ */
+export function clearTracking(link: Link) {
+  while (link) {
+    const { nextDep, nextSub, prevSub, dep } = link
+    /**
+     * 如果prevSub有，那就把prevSub的下一个节点只想当前节点的下一个节点
+     * 如果没有，那就是头节点，那就把dep.subs指向当前节点的下一个
+     */
+    if (prevSub) {
+      prevSub.nextSub = nextSub
+      link.nextSub = undefined
+    } else {
+      dep.subs = nextSub
+    }
+
+    /**
+     * 如果nextSub有，那就把nextSub的上一个节点只想当前节点的上一个节点
+     * 如果nextSub没有，那就是尾节点，把depsTail指向当前节点的上一个节点
+     */
+    if (nextSub) {
+      nextSub.prevSub = prevSub
+      link.prevSub = undefined
+    } else {
+      dep.subsTail = prevSub
+    }
+
+    link.dep = link.sub = undefined
+
+    link.nextDep = undefined
+    link = nextDep
+  }
 }
